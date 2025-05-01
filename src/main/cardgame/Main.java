0354add35c144c51fd5969ff2da9aee1d5de2e43
dat.petr@gmail.com <dat.petr@gmail.com>
2025-05-01 12:24:25 +0300

    crating the folder structure for the app

---
+++ src/main/cardgame/Main.java
@@ -0,0 +1,8 @@
+package main.cardgame;
+
+public class Main {
+    public static void main(String[] args) {
+        // entry point for the application
+        // create and start the game
+    }
+}
