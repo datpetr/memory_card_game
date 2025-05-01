e3c4e1d6dee0674ea3f7b9fb8660834837c7b000
datpetr <dat.petr@gmail.com>
2025-05-01 13:49:50 +0300

    creating modes

--- src/main/cardgame/model/GameBoard.java
+++ src/main/cardgame/model/GameBoard.java
@@ -10,18 +10,22 @@ public class GameBoard {
         this.size = size;
         this.board = new Card[size][size];
         // Initialize the board with cards
+        // will be added more code later
     }
 
     public Card getCard(int row, int col) {
         return board[row][col];
+        // will be added more code later
     }
 
     public void displayBoard() {
         // Display the current state of the board
+        // will be added more code later
     }
 
     public boolean allCardsMatched() {
         // Check if all cards on the board are matched
+        // will be added more code later
         return false;
     }
 }
--- src/main/cardgame/util/Timer.java
+++ src/main/cardgame/util/Timer.java
@@ -10,13 +10,16 @@ public class Timer {
 
     public void startTimer() {
         startTime = System.currentTimeMillis();
+        // will be added more code later
     }
 
     public void stopTimer() {
         endTime = System.currentTimeMillis();
+        // will be added more code later
     }
 
     public long getElapsedTime() {
         return endTime - startTime;
+        // will be added more code later
     }
 }
