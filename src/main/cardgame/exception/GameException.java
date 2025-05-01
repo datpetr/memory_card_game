a2b34d5934cf1db25182ee62ca5b5b2d2645b8d6
datpetr <dat.petr@gmail.com>
2025-05-01 13:36:43 +0300

    creating skeleton

---
+++ src/main/cardgame/exception/GameException.java
@@ -0,0 +1,7 @@
+package main.cardgame.exception;
+
+public class GameException extends Exception {
+    public GameException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/cardgame/model/DeckTest.java
@@ -0,0 +1,5 @@
+package test.cardgame.model;
+
+public class DeckTest {
+    // Unit tests for Deck class functionality
+}
