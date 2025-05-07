232ced80696142d0017e80978c22f217d7f14680
AhmetM16 <ahmetm.turac@gmail.com>
2025-05-08 01:41:56 +0300

    lil upd. on the deck class to add and shuffle cards

--- src/main/cardgame/model/Deck.java
+++ src/main/cardgame/model/Deck.java
@@ -1,7 +1,8 @@
 package main.cardgame.model;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Deck {
     private List<Card> cards;
@@ -9,10 +10,14 @@ public class Deck {
     public Deck(String[] values) {
         // Create a deck of cards with pairs of values
         cards = new ArrayList<>();
+        for(int i = 0; i < values.length; i++){
+            cards.add(new Card(i, values[i])); //adding each card their unique ID and value
+        }
         // Initialize cards
     }
 
     public void shuffle() {
+        Collections.shuffle(cards); 
         // Shuffle the deck
     }
 
