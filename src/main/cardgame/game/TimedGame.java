aa77b243b4e8b7adf9cfd3097689564dc7ce4f78
Azil-heh <e265658@metu.edu.tr>
2025-05-08 10:09:25 +0300

    creating modes and further updates

--- src/main/cardgame/game/TimedGame.java
+++ src/main/cardgame/game/TimedGame.java
@@ -7,7 +7,7 @@ import main.cardgame.util.Timer;
 public class TimedGame extends Game {
     public static final int EASY_TIME = 5 * 60;   // 5 minutes
     public static final int MEDIUM_TIME = 3 * 60; // 3 minutes
-    public static final int HARD_TIME = 60;
+    public static final int HARD_TIME = 1* 60;
 
     private boolean isStopped;
     private Timer timer;// 1 minute
